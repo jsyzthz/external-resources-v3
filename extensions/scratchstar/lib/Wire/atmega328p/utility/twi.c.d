1b2219104beebbd3b6aaa52dd39bb3a98187b44a
Tong Huazhong <jsyzthz@gmail.com>
2025-06-17 19:02:00 +0800

    删除多余

--- extensions/scratchstar/lib/Wire/atmega328p/utility/twi.c.d
+++
@@ -1,6 +0,0 @@
-/mnt/itinman-data/.cache/arduino/sketches/93022C26729E2B64527AE294C433621E/libraries/Wire/utility/twi.c.o: \
- /mnt/itinman-data/.arduino15/packages/arduino/hardware/avr/1.8.6/libraries/Wire/src/utility/twi.c \
- /mnt/itinman-data/.arduino15/packages/arduino/hardware/avr/1.8.6/cores/arduino/Arduino.h \
- /mnt/itinman-data/.arduino15/packages/arduino/hardware/avr/1.8.6/cores/arduino/binary.h \
- /mnt/itinman-data/.arduino15/packages/arduino/hardware/avr/1.8.6/variants/mega/pins_arduino.h \
- /mnt/itinman-data/.arduino15/packages/arduino/hardware/avr/1.8.6/libraries/Wire/src/utility/twi.h
